81553ec623787841bac4af07e4ff10e4f24121fd
Farhan Poh-Asae <farhan.abuhazan@gmail.com>
2021-07-09 16:09:13 +0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
-import FlowBuilder, { Elements, NodeElement } from "./pages/flow/FlowBuilder";
-import { ReactFlowProvider, useStoreState } from "react-flow-renderer";
-import { FlowBlocks } from "./pages/flow/FlowBlocks";
-import { BlockEditor } from "./pages/flow/NodeEditor";
-import { useEffect } from "react";
+import React, {useState} from "react";
+import {Box, Button, Flex, Text} from "@chakra-ui/react";
+import FlowBuilder, {Elements, NodeElement} from "./pages/flow/FlowBuilder";
+import {ReactFlowProvider, useStoreState} from "react-flow-renderer";
+import {FlowBlocks} from "./pages/flow/FlowBlocks";
+import {BlockEditor} from "./pages/flow/NodeEditor";
+import {useEffect} from "react";
 import {gql, useMutation} from "@apollo/client";
 
 const SaveButton = () => {
@@ -29,7 +29,7 @@ const SaveButton = () => {
       variant="outline"
       textAlign="center"
       onClick={() => {
-        console.debug("Mutation With These Variable:", { nodes, edges });
+        console.debug("Mutation With These Variable:", {nodes, edges});
         setLoading(true);
       }}
     >
@@ -38,20 +38,60 @@ const SaveButton = () => {
   );
 };
 
-const TriggerEvent = ({name}: {name: string}) => {
+const TeamStoreEvent = ({name}: { name: string }) => {
   const [isLoading, setLoading] = useState(false);
-  const [mTriggerEvent] = useMutation(gql`
-    mutation publishEvent {
-      publishEvent(input: {
-        eventType: "TEAMSTORE",
-        payload: {
-          storeId:"store-3"
+    const [mTriggerEvent] = useMutation(gql`
+        mutation publishEvent {
+            publishEvent(input: {
+                eventType: "INVENTORY",
+                payload: {
+                    storeId:"store-1"
+                }
+            }){
+                success
+            }
         }
-      }){
-        success
-      }
-    }
-  `)
+    `)
+
+  useEffect(() => {
+    if (!isLoading) return;
+    let timer1 = setTimeout(() => setLoading(false), 3000);
+
+    return () => {
+      clearTimeout(timer1);
+    };
+  }, [isLoading]);
+  return (
+    <Button
+      w="8rem"
+      isLoading={isLoading}
+      variant="outline"
+      textAlign="center"
+      onClick={async () => {
+        await mTriggerEvent()
+        setLoading(true);
+      }}
+    >
+      <Text fontWeight="bold">{name || 'default'}</Text>
+    </Button>
+  );
+};
+
+const InventoryEvent = ({name}: { name: string }) => {
+  const [isLoading, setLoading] = useState(false);
+    const [mTriggerEvent] = useMutation(gql`
+        mutation publishEvent {
+            publishEvent(input: {
+                eventType: "TEAMSTORE",
+                payload: {
+                    storeId:"store-3"
+                }
+            })
+            {
+                success
+            }
+        }
+    `)
 
   useEffect(() => {
     if (!isLoading) return;
@@ -87,17 +127,18 @@ function App() {
         <Flex w="100%" h="100%" flexDirection="row">
           <Flex flexBasis="20%" justifyContent="center" p="2rem">
             <Flex flexDirection="column" alignContent="space-around">
-              <FlowBlocks />
+              <FlowBlocks/>
               <Box w="100%" textAlign="center" mb="24rem">
-                <SaveButton />
+                <SaveButton/>
               </Box>
               <Box w="100%" textAlign="center" mb="24rem">
-                <TriggerEvent name="Trigger Event"/>
+                <InventoryEvent name="Teamstore Event"/>
+                <TeamStoreEvent name="Inventory Event"/>
               </Box>
             </Flex>
           </Flex>
           <Flex flexBasis="60%" border="1px solid red">
-            <FlowBuilder {...{ elements, setElements, setNodeId }} />
+            <FlowBuilder {...{elements, setElements, setNodeId}} />
           </Flex>
           <Flex
             flexBasis="20%"
@@ -105,7 +146,7 @@ function App() {
             alignItems="center"
             p="2rem"
           >
-            <BlockEditor nodeId={nodeId} setElements={setElements} />
+            <BlockEditor nodeId={nodeId} setElements={setElements}/>
           </Flex>
         </Flex>
       </Box>
